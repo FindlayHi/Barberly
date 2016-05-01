b2012c22017e391450fabe6dc3f8b3dcc2f28460
ChristianHernon <w0223388@nscc.ca>
2016-05-01 00:19:15 -0300

    fixed routes for CRUD

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -38,6 +38,26 @@ angular
         controller: 'BarbersCtrl',
         controllerAs: 'barbers'
       })
+      .when('/create/barber', {
+        templateUrl: 'views/barber-add.html',
+        controller: 'BarberAddCtrl',
+        // controllerAs: 'barbersAdd'
+      })
+      .when('/barber/:id', {
+        templateUrl: 'views/barber-view.html',
+        controller: 'BarberViewCtrl',
+        //controllerAs: 'barbers'
+      })
+      .when('/barber/:id/edit', {
+        templateUrl: 'views/barber-edit.html',
+        controller: 'BarberEditCtrl',
+        //controllerAs: 'barbers'
+      })
+      .when('/barber/:id/delete', {
+        templateUrl: 'views/barber-delete.html',
+        controller: 'BarberDeleteCtrl',
+        //controllerAs: 'barbers'
+      })
       .when('/appointments', {
         templateUrl: 'views/appointments.html',
         controller: 'AppointmentsCtrl',
@@ -52,6 +72,86 @@ angular
         templateUrl: 'views/services.html',
         controller: 'ServicesCtrl',
         controllerAs: 'services'
+      })
+      .when('/appointment/:id/edit', {
+        templateUrl: 'views/appointment-edit.html',
+        controller: 'AppointmentEditCtrl',
+        controllerAs: 'appointmentEdit'
+      })
+      .when('/create/barber', {
+        templateUrl: 'views/barber-add.html',
+        controller: 'BarberAddCtrl',
+        controllerAs: 'barberAdd'
+      })
+      .when('/barber/:id', {
+        templateUrl: 'views/barber-view.html',
+        controller: 'BarberViewCtrl',
+        controllerAs: 'barberView'
+      })
+      .when('/barber/:id/edit', {
+        templateUrl: 'views/barber-edit.html',
+        controller: 'BarberEditCtrl',
+        controllerAs: 'barberEdit'
+      })
+      .when('/barber/:id/delete', {
+        templateUrl: 'views/barber-delete.html',
+        controller: 'BarberDeleteCtrl',
+        controllerAs: 'barberDelete'
+      })
+      .when('/client/:id', {
+        templateUrl: 'views/client-view.html',
+        controller: 'ClientViewCtrl',
+        controllerAs: 'clientView'
+      })
+      .when('/client/:id/edit', {
+        templateUrl: 'views/client-edit.html',
+        controller: 'ClientEditCtrl',
+        controllerAs: 'clientEdit'
+      })
+      .when('/client/:id/delete', {
+        templateUrl: 'views/client-delete.html',
+        controller: 'ClientDeleteCtrl',
+        controllerAs: 'clientDelete'
+      })
+      .when('/service/:id/delete', {
+        templateUrl: 'views/service-delete.html',
+        controller: 'ServiceDeleteCtrl',
+        controllerAs: 'serviceDelete'
+      })
+      .when('/service/:id/edit', {
+        templateUrl: 'views/service-edit.html',
+        controller: 'ServiceEditCtrl',
+        controllerAs: 'serviceEdit'
+      })
+      .when('/service/:id', {
+        templateUrl: 'views/service-show.html',
+        controller: 'ServiceShowCtrl',
+        controllerAs: 'serviceShow'
+      })
+      .when('/service/:id', {
+        templateUrl: 'views/service-view.html',
+        controller: 'ServiceViewCtrl',
+        controllerAs: 'serviceView'
+      })
+      .when('/create/service', {
+        templateUrl: 'views/service-add.html',
+        controller: 'ServiceAddCtrl',
+        controllerAs: 'serviceAdd'
+      })
+      .when('/appointment/:id', {
+        templateUrl: 'views/appointment-view.html',
+        controller: 'AppointmentViewCtrl',
+        controllerAs: 'appointmentView'
+      })
+      .when('/appointment/:id/delete', {
+        templateUrl: 'views/appointment-delete.html',
+        controller: 'AppointmentDeleteCtrl',
+        controllerAs: 'appointmentDelete'
+      })
+      .when('/create/appointment', {
+        templateUrl: 'views/appointment-add.html',
+        controller: 'AppointmentAddCtrl',
+        controllerAs: 'appointmentAdd'
       })
       .otherwise({
         redirectTo: '/'
--- client/app/scripts/controllers/barber-delete.js
+++ client/app/scripts/controllers/barber-delete.js
@@ -9,7 +9,7 @@
  */
 angular.module('clientApp')
   .controller('BarberDeleteCtrl', function($scope, $routeParams, Barber, $location) {
-    $scope.barber = Barber.one($routeParam.id).get().$object;
+    $scope.barber = Barber.one($routeParams.id).get().$object;
     $scope.deleteBarber = function() {
       $scope.barber.remove().then(function() {
         $location.path('/barbers');
